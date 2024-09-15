94624f8c661be43fc57153c3aa7757e4c134936b
nikola <nikodola@gmail.com>
2024-09-15 19:50:58 +0200

    second commit

--- src/app/(admin)/report-accident/page.jsx
+++ src/app/(admin)/report-accident/page.jsx
@@ -20,7 +20,7 @@ export default async function Admin(){
             description,
             time,
             department,
-            lastAccident,
+            lastAccident, 
         })
     }
     
